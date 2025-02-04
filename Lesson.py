4b383d23ee3457ed205292bc950a0f0db392e7ce
rayysvv <rayysvv@gmail.com>
2025-02-04 10:04:41 -0500

    Completed

--- Lesson.py
+++ Lesson.py
@@ -1,7 +1,7 @@
-sen1 = input("Enter a sentence: ")
+""" sen1 = input("Enter a sentence: ")
 
 count_word = sen1.split( )
-print(f"The number of words in the sentence is: {len(count_word)}")
+print(f"The number of words in the sentence is: {len(count_word)}") """
 
 
 #Challenge 1
@@ -21,10 +21,8 @@ tip_mapping = {
 tip = tip_mapping.get(bill, "Invalid input")
 print(f"Your tip is {tip} please") """
 
-#Challenge 3
-""" import math
-
-number1 = int(input("Enter a number: "))
+#Challenge 350
+""" number1 = int(input("Enter a number: "))
 
 def find_factors(number1):
     factors = []
@@ -37,18 +35,7 @@ factors = find_factors(number1)
 print(f"The factors of {number1} are: {factors}") """
 
 #Challenge 4
-""" import math
-
-def gcf(number1, number2):
-    return math.gcd(number1, number2)
-
-number1 = int(input("Enter a number: "))
-number2 = int(input("Enter another number: "))
-
-gcf= gcf(number1, number2)
-print("The GCF is: ", gcf) """
-
-number1 = int(input("Enter a number: "))
+""" number1 = int(input("Enter a number: "))
 number2 = int(input("Enter another number: "))
 
 def gcf(number1, number2):
@@ -57,4 +44,4 @@ def gcf(number1, number2):
     return number1
 
 gcf_value = gcf(number1, number2)
-print(f"The GCF of {number1} and {number2} is: {gcf_value}")
+print(f"The GCF of {number1} and {number2} is: {gcf_value}") """
--- Madlib Project.py
+++ Madlib Project.py
@@ -6,13 +6,13 @@ noun = input("Enter a noun: ")
 verb2 = input("Enter a verb2: ")
 number = input("Enter a number: ")
 
-noun2 = input("Enter a noun: ")
-verb3 = input("Enter a verb: ")
+noun2 = input("Enter a noun2: ")
+verb3 = input("Enter a verb3: ")
 
-number2 = input("Enter a number: ")
+number2 = input("Enter a number2: ")
 
 celebrity = input("Enter a celebrity: ")
 
-verb4 = input("Enter a verb: ")
+verb4 = input("Enter a verb4: ")
 
 print(f"I {verb} {noun} and then I {verb2} to the grocery store and brought {number} {noun2}. When I {verb3} home, I saw {number2} {celebrity} and then I {verb4}. End Of Story")
